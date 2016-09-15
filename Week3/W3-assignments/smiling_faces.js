8987a95e521125780928b7dd9ec2592c747fd220
carello <cp2carello@gmail.com>
2016-09-15 13:15:45 -0500

    adding week3 assignments part 1 and part2 completed
    	new file:   part1.html
    	new file:   part2.html
    	new file:   smile.png
    	new file:   smiling_faces.js

---
+++ Week3/W3-assignments/smiling_faces.js
@@ -0,0 +1,14 @@
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+
+function generateFaces() {
+    for (var i=0; i < numberOfFaces; i++) {
+        var make_faces = document.createElement("img");
+        var random_spot_top = Math.random() * 400;
+        var random_spot_left = Math.random() * 400;
+        make_faces.src="smile.png";
+        make_faces.style.top = random_spot_top + "px";
+        make_faces.style.left = random_spot_left + "px";
+        theLeftSide.appendChild(make_faces)[i];
+      }
+}
